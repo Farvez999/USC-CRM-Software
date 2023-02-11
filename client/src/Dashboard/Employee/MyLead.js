8c5f3b056880ccc28e487b46ecac86c0237a4bdd
Farvez Hossen <56191538+Farvez999@users.noreply.github.com>
2023-02-11 18:37:41 +0600

    Edit button added

--- client/src/Dashboard/Employee/MyLead.js
+++ client/src/Dashboard/Employee/MyLead.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthProvider';
 import { useQuery } from '@tanstack/react-query'
@@ -7,33 +7,47 @@ const MyLead = () => {
 
     const { user } = useContext(AuthContext)
     console.log(user)
-    const [isloader, setIsloader] = useState(false)
 
-    const navigate = useNavigate()
+    const [leads, setLeads] = useState([])
+    // console.log(leads)
 
-    const { data: products, isLoading, refetch } = useQuery({
-        queryKey: ['products'],
-        queryFn: async () => {
-            try {
-                const res = await fetch(`http://localhost:5000/leads/${user.displayName}`, {
-                    headers: {
-                        authorization: `bearer ${localStorage.getItem('accessToken')}`
-                    }
-                });
-                const data = await res.json();
-                return data;
-            }
-            catch (error) {
 
-            }
+    fetch(`http://localhost:5000/leads/${user.displayName}`, {
+        headers: {
+            authorization: `bearer ${localStorage.getItem('accessToken')}`
         }
-    });
+    })
+        .then((response) => response.json())
+        .then((data) => setLeads(data))
 
-    console.log(products);
+
+    // const { data: products, isLoading, refetch } = useQuery({
+    //     queryKey: ['products'],
+    //     queryFn: async () => {
+    //         try {
+    //             const res = await fetch(`http://localhost:5000/leads/${user.displayName}`, {
+    //                 headers: {
+    //                     authorization: `bearer ${localStorage.getItem('accessToken')}`
+    //                 }
+    //             });
+    //             const data = await res.json();
+    //             return data;
+    //         }
+    //         catch (error) {
+
+    //         }
+    //     }
+    // });
+
+    const [updateState, setUpdateState] = useState(-1)
+
+    // const handleEdit = (id) => {
+
+    // }
 
     return (
         <div>
-            <h3 className="text-3xl mb-5">My Leads : {products?.data?.length}</h3>
+            <h3 className="text-3xl mb-5">My Leads : {leads?.data?.length}</h3>
 
             <div className="overflow-x-auto">
                 <table className="table w-full">
@@ -51,59 +65,69 @@ const MyLead = () => {
                             <th>Remark</th>
                             <th>RemarkTwo</th>
                             <th>AdmissionStates</th>
+                            <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
 
                         {
-                            products?.data?.map((product, i) => <tr key={product._id}>
-                                <th>{i + 1}</th>
-                                <td>{product.Name}</td>
-                                <td>{product?.Phone}</td>
-                                <td>{product.Email}</td>
-                                <td>
-                                    <input type="" name="" value={product.FirstFollowup}></input>
-                                </td>
-                                <td>{product.SecondFollowup}</td>
-                                <td>{product.ThirdFollowup}</td>
-                                <td>{product.NextFollowupDate}</td>
-                                <td>{product.Remark}</td>
-                                <td>{product.RemarkTwo}</td>
-                                <td>{product.AdmissionStates}</td>
+                            leads?.data?.map((lead, i) =>
+                                updateState === lead.Id ? <Edit lead={lead} leads={leads} setLeads={setLeads} /> :
+                                    <tr
+                                        key={lead.Id}>
+                                        <th>{i + 1}</th>
+                                        <td>{lead.Name}</td>
+                                        <td>{lead?.Phone}</td>
+                                        <td>{lead.Email}</td>
+                                        <td>{lead.FirstFollowup}</td>
+                                        <td>{lead.SecondFollowup}</td>
+                                        <td>{lead.ThirdFollowup}</td>
+                                        <td>{lead.NextFollowupDate}</td>
+                                        <td>{lead.Remark}</td>
+                                        <td>{lead.RemarkTwo}</td>
+                                        <td>{lead.AdmissionStates}</td>
+                                        <td>
+                                            <button onClick={() => handleEdit(lead.Id)} className="btn btn-sm btn-primary mr-2">Edit</button>
+                                            <button className="btn btn-sm btn-secondary">Delete</button>
+                                        </td>
 
-                                {/* {
 
-                                product.paid ? <td className='text-red-600'>{'sold'}</td> :
-                                    product.advertise ?
-                                        <td><button className="btn btn-outline btn-error btn-xs" onClick={() => removeAdvertise(product._id)}>remove advertise</button></td>
-
-                                        :
-
-                                        <td><button className="btn  btn-primary btn-xs" onClick={() => addAdvertise(product._id)}>add advertise</button></td>
-
-                            }
-                            <td>
+                                        {/* <td>
                                 <label onClick={() => handleDelete(product)} htmlFor="confirmation-modal" className="btn btn-sm btn-error">Delete</label>
-                            </td> */}
-                            </tr>)
+                            </td>  */}
+                                    </tr>)
                         }
 
                     </tbody>
                 </table>
             </div>
-            {/* {
-            deletingDoctor && <ConfirmastionModal
-                title={`Are you sure you want to delete?`}
-                message={`If you delete ${deletingDoctor.name}. It cannot be undone.`}
-                successAction={handleDeleteDoctor}
-                successButtonName="Delete"
-                modalData={deletingDoctor}
-                closeModal={closeModal}
-            >
-            </ConfirmastionModal>
-        } */}
+
         </div>
     );
+
+    function handleEdit(id) {
+        setUpdateState(id)
+    }
 };
+
+function Edit({ lead, leads, setLeads }) {
+    console.log(lead);
+    return (
+        <tr>
+            <th>{1}</th>
+            <td>{lead.Name}</td>
+            <td>{lead?.Phone}</td>
+            <td>{lead.Email}</td>
+            <td><input type="text" name="name" value={lead.FirstFollowup}></input></td>
+            <td><input type="text" name="name" value={lead.SecondFollowup}></input></td>
+            <td>{lead.ThirdFollowup}</td>
+            <td>{lead.NextFollowupDate}</td>
+            <td>{lead.Remark}</td>
+            <td>{lead.RemarkTwo}</td>
+            <td>{lead.AdmissionStates}</td>
+            <button type='submit' className="btn btn-sm btn-primary mr-2">Edit</button>
+        </tr>
+    )
+}
 
 export default MyLead;
